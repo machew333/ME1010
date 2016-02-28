3d5623d581d363385d615e9340abccba551dae07
machew22 <machew333@gmail.com>
2016-02-27 22:30:01 -0700

    Big refactor
    Refactored all of the Launch Tester.
    Also am implementing LockednLoaded auto fire sketch.

--- LaunchTester/LaunchTester.ino
+++ LaunchTester/LaunchTester.ino
@@ -8,7 +8,7 @@ int servoAngle;
 
 // Servo Realoader
 const int reloaderServoPin = 10;
-Servo reloaderServo;  
+Servo reloaderServo;
 
 
 int solPowPin = 6; //Solenoid power pin
@@ -22,21 +22,21 @@ int angleToDropBall = 0; //Y
 void setup() {
   //Serial
   Serial.begin(9600);
-  
+
   //Servo motor control
   angleServo.attach(servoPin);
   servoAngle = 0;
 
   //Solenoid
-  pinMode(solDirPin,OUTPUT);
-  pinMode(solPowPin,OUTPUT);
+  pinMode(solDirPin, OUTPUT);
+  pinMode(solPowPin, OUTPUT);
   digitalWrite(solDirPin, HIGH);
 
   //Servo Reloader
-  reloaderServo.attach(reloaderServoPin);  
-  Serial.begin(9600);     
-  reloaderServo.write(0);  
-  reloaderServo.write(angleToLoadNextBall); 
+  reloaderServo.attach(reloaderServoPin);
+  Serial.begin(9600);
+  reloaderServo.write(0);
+  reloaderServo.write(angleToLoadNextBall);
 }
 
 void loop() {
@@ -50,23 +50,106 @@ void loop() {
     angleServo.write(servoAngle);
   }
 
- printDisplay();
+  printDisplay();
 }
 
 
-void launchSequence() {
+
+
+//---------------------------------\\
+//------- METHODS WE MADE ---------\\
+//---------------------------------\\
+
+
+
+//Fire all ammo 
+void unloadAmmo() {
+  int shotTime = 400;
   int launchDelay = 500;
-  int onTime = 400;
-  Serial.println("Launch");
-  analogWrite(solPowPin, solenoidPower);
-  delay(onTime);
-  analogWrite(solPowPin,0);
-  delay(launchDelay);
-  reload();
+  
+  int numBalls = 6;
+
+  for (int i =0; i < numBalls -1 ; i++) {
+    String shotText = String("Shot") + i + String(" out");
+    
+    int launchDelay = 500;
+    int onTime = 400;
+    Serial.println("Launch");
+    analogWrite(solPowPin, solenoidPower);
+    delay(onTime);
+    analogWrite(solPowPin, 0);
+    delay(launchDelay);
+    reload();
+  }  
 }
 
 
 
+//Drop ball to reload cannon
+void reload() {
+
+  int firingAngle = angleServo.read();
+  angleServo.write(84);
+  delay(800);
+  reloaderServo.write(angleToDropBall);
+  Serial.println("Drop ball");
+  delay(800);
+  angleServo.write(firingAngle);
+  delay(800);
+  reloaderServo.write(angleToLoadNextBall);
+  Serial.println("Reload");
+}
+
+
+
+
+//SERVO ADJUSTMENTS
+
+int adjustDelay = 500;
+void decreaseAngle() {
+  Serial.println("Change angle");
+  if (servoAngle > 0) {
+    servoAngle -= 10;
+    delay(adjustDelay);
+  }
+}
+void increaseAngle() {
+  if (servoAngle < 180) {
+    servoAngle += 10;
+    delay(adjustDelay);
+  }
+}
+
+
+
+
+/*
+ * I/O METHODS SECTION
+ */
+
+void handleButtonClick(int buttonPressed) {
+  //Button frequency ranges for Romeo board
+  int up[2] = {0, 100};
+  int down[2] = {300, 350};
+  int launchButton[2] = {725, 775};
+  //int ranges[3][2] = {down,up,launch};
+
+  //down
+  if (buttonPressed >= down[0] && buttonPressed <= down[1]) {
+    decreaseAngle();
+  }
+  //up
+  else if (buttonPressed >= up[0] && buttonPressed <= up[1]) {
+    increaseAngle();
+  }
+  //launch
+  else if (buttonPressed >= launchButton[0] && buttonPressed <= launchButton[1])  {
+    unloadAmmo();
+  }
+
+}
+
+//Print servo angle 
 void printDisplay() {
   //Serial.print(analogRead(A7));
   Serial.print("servoAngle = ");
@@ -74,66 +157,10 @@ void printDisplay() {
 }
 
 
-int adjustDelay = 500;
-void decreaseAngle() {
-  Serial.println("Change angle");
-  if (servoAngle >0) {
-    servoAngle -= 5;
-    delay(adjustDelay);
-  }
-}
-void increaseAngle() {
-  if (servoAngle<180) {
-    servoAngle += 5;
-    delay(adjustDelay);
-  }
-}
 
-void handleButtonClick(int buttonPressed) {
- //Button frequency ranges for Romeo board
-int up[2]= {0,100};
-int down[2] = {300,350};
-int launchButton[2] = {725, 775};
-//int ranges[3][2] = {down,up,launch};
 
-  //Down button pressed
-  if (buttonPressed >= down[0] && buttonPressed <= down[1]) {
-    decreaseAngle();
-  }
-  //Up button pressed
-  else if (buttonPressed >= up[0] && buttonPressed <= up[1]) {
-    increaseAngle();
-  }
-  else if (buttonPressed >= launchButton[0] && buttonPressed <= launchButton[1])  {
-    
-    launchSequence();
-    delay(1000);
-    launchSequence();
-    delay(1000);
-    launchSequence();
-    delay(1000);
-    launchSequence();
-    delay(1000);
-    launchSequence();
-    delay(1000);
-    launchSequence();
-  }
-}
- 
- //Reloading Method
-  void reload(){
- 
-  int firingAngle = angleServo.read();
-  angleServo.write(84);
-  delay(800);
-  reloaderServo.write(angleToDropBall);
-  Serial.println("Drop ball");
-  delay(1000);
-  angleServo.write(firingAngle);
-  reloaderServo.write(angleToLoadNextBall);
-  Serial.println("Reload");
-  
- }
+
+
  
 
 
