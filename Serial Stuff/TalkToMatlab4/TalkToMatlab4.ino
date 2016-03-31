a22194ec2a7cce689b511dcedceb97470757cde5
KevinMoffatt <kmoffatt27@sbcglobal.net>
2016-03-31 15:03:29 -0600

    Matlab Romeo Comm 4
    working on matlab romeo comm 4

---
+++ Serial Stuff/TalkToMatlab4/TalkToMatlab4.ino
@@ -0,0 +1,28 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); //baud must match matlab side
+  Serial.write(' ');
+
+  int lenTargets = 6;
+  byte matlabData[3];
+  char reply[50];
+
+  for (int i = 0; i<lenTargets; i++) {
+
+    while (!Serial.available()) {
+      continue; //do nothing
+    }
+    Serial.readBytes(matlabData,3);
+    endoderPos = matlabData[0];
+    xTarget_HB = matlabData[1];
+    xTarget_LB = matlabData[2];
+    targetNumber = i+1;
+    sprintf(reply,"For target %d, I recieved encoderPos = %d, xTarget_HB = %d, and xTarget_LB = %d",targetNumber,encoderPos,xTarget_HB,xTarget_LB);
+    Serial.println(reply);      
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Serial Stuff/TalkToRomeo4.m
@@ -0,0 +1,67 @@
+% Script for Serial communication to Arduino(Romeo) board
+
+%try and catch to make sure serial isn't left on
+try 
+
+%Ensure there are no previous communications
+if (length(instrfind) > 0)
+    fclose(instrfind);
+    delete(instrfind);
+end
+
+%Initialize port
+windowsPort = 'COM23';
+macPort = '/dev/cu.usbmodem1411';
+
+%speed of serial transmission. must match Arduino side
+baudRate = 9600;
+
+%Serial is an object that represents the serial port connection
+Serial = serial(macPort,'BAUD',baudRate);
+fopen(Serial);
+
+%Wait for Romeo to send a message
+fprintf('\nWaiting for Romeo...\n')
+while (~ Serial.BytesAvailable)
+    continue;
+end
+fprintf('found him\n');
+
+targets = 1:6;
+
+rowVal = linspace(100,400,6);
+colVal = linspace(550,50,6);
+encoderPos = (rowVal/10)-6;
+xTarget_mm = colVal + 650;
+xTarget_HB = floor(xTarget_mm/256);
+xTarget_LB = xTarget_mm - 256 * xTarget_HB;
+
+for i = targets
+    values = [encoderPos,xTarget_HB,xTarget_LB];
+    fwrite(Serial,values);
+    %Scan for message
+    arduinoMessage = fscanf(Serial);
+    fprintf('\n%s\n',arduinoMessage);
+end
+
+
+%Close the communications
+fclose(Serial); %closes Serial port
+delete(Serial); %removes Serial from memory
+clear Serial;
+
+fprintf('Done\n');
+
+catch 
+    %in case thing quits, close serial communications
+    fclose(Serial);
+end
+
+
+
+
+
+
+
+
+    
